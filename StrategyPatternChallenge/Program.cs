a4b3e3b70f5e2c4bad0660dedad9962d6753b201
Julio Villa <jvilla@apexsystems.com>
2026-02-19 13:25:59 -0600

    Created factory to create strategy

--- StrategyPatternChallenge/Program.cs
+++ StrategyPatternChallenge/Program.cs
@@ -1,10 +1,41 @@
 ﻿using StrategyPatternChallenge.Strategies;
+using System.Reflection.Metadata;
 
 namespace StrategyPatternChallenge;
 
 internal class Program
 {
     private static void Main(string[] args)
+    {
+        UseSimpleSelector();
+
+        UseFactory();
+
+        Console.ReadKey();
+    }
+
+    private static void UseFactory()
+    {
+        // Step 1: Create an order with a total weight and a shipping method
+        //var order = new Order(10, "UPS");
+        // var order = new Order(10, "FEDEX");
+         var order = new Order(10, "USPS");
+        //var order = new Order(10, "DHL");
+
+        // Step 2: Determine the appropriate shipping strategy based on the order's shipping method
+        IShippingStrategy strategy = ShippingStrategyFactory.Create(order.ShippingMethod);
+
+        // Step 3: Inject the selected strategy into the calculator (context)
+        var calculator = new ShippingCalculator(strategy);
+
+        // Step 4: Delegate the shipping cost calculation to the strategy
+        var cost = calculator.CalculateShippingCost(order);
+
+        // Step 5: Output the calculated shipping cost
+        Console.WriteLine($"Shipping cost for {order.ShippingMethod}: {cost}");
+    }
+
+    private static void UseSimpleSelector()
     {
         // Step 1: Create an order with a total weight and a shipping method
         //var order = new Order(10, "UPS");
@@ -41,7 +72,5 @@ internal class Program
 
         // Step 5: Output the calculated shipping cost
         Console.WriteLine($"Shipping cost for {order.ShippingMethod}: {cost}");
-
-        Console.ReadKey();
     }
 }
---
+++ StrategyPatternChallenge/ShippingStrategyFactory.cs
@@ -0,0 +1,24 @@
+﻿using StrategyPatternChallenge.Strategies;
+
+namespace StrategyPatternChallenge
+{
+    public static class ShippingStrategyFactory
+    {
+        public static IShippingStrategy Create(string shippingMethod)
+        {
+            switch (shippingMethod)
+            {
+                case "UPS":
+                    return new UpsStrategy();
+                case "FEDEX":
+                    return new FedexStrategy();
+                case "USPS":
+                    return new UspsStrategy();
+                case "DHL":
+                    return new DhlStrategy();
+                default:
+                    throw new InvalidOperationException("Unsupported shipping method");
+            }
+        }
+    }
+}
